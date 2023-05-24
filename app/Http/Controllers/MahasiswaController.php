998a0d455a8009f7e55e2940e56d0b5e19e4dd22
Cahzello <rizkyrasya.m@gmail.com>
2023-05-24 09:15:25 +0700

    update

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
     public function index(){
-        return "tabel mahasiswa disini";
+        $mahasiswas = Mahasiswa::all();
+        return view('mahasiswa.index', ['mahasiswas' => $mahasiswas]);
     }
 
     public function create(){
@@ -22,6 +23,17 @@ class MahasiswaController extends Controller
             'jurusan' => 'required',
             'alamat' => '',
         ]);
-        dump($validateData);
+
+        $mahasiswa = new Mahasiswa();
+        $mahasiswa->nim = $validateData['nim'];
+        $mahasiswa->nama = $validateData['nama'];
+        $mahasiswa->jenis_kelamin = $validateData['jenis_kelamin'];
+        $mahasiswa->jurusan = $validateData['jurusan'];
+        $mahasiswa->alamat = $validateData['alamat'];
+        $mahasiswa->save();
+
+        return "data berhasil diinput ke database";
+
+
     }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="\css\bootstrap.min.css">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-12">
+                
+                <div class="py-4">
+                    <h2>Tabel Mahasiswa</h2>
+                </div>
+
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nim</th>
+                            <th>Nama</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Jenis Juruasn</th>
+                            <th>Alamat</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
